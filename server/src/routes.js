2b1c20306a073c26d24d3bbb3b8bdf5e9573fcbf
LungPong <pongpee1@gmail.com>
2021-04-20 21:45:02 +0700

    delete tutorail file from server folder

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,6 @@
 const DatabaseController = require('./controllers/DatabaseControllers')
 
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-        message: `Hello ${req.body.email}!, Your account was registered! Have fun!`
-    })
-  })
-
   app.get('/user', DatabaseController.getAllName)
   app.post('/user', DatabaseController.getAllDataByName)
   app.put('/user', DatabaseController.updateUserTemp)
