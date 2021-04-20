24c74297bc1a9312a7bc72c9ecffc6d95a080150
LungPong <pongpee1@gmail.com>
2021-04-20 19:48:46 +0700

    add serve-static lib

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const config = require('./configs/config')
+const serveStatic = require('serve-static')
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('combined'))
 app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(serveStatic("public"));
+}
 
 require('./routes')(app)
 
