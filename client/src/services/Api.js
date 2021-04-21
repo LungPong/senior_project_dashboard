57a3ddeaed4001ec192fe4537af1fdadda429705
LungPong <pongpee1@gmail.com>
2021-04-21 20:54:46 +0700

    change baseURL that call api

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:9070`
+    // baseURL: `http://localhost:9070`
+    baseURL: `https://wtfss-dashboard.herokuapp.com`
   })
 }
