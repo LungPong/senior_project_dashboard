b5737fefa47bb99f740a0dff95fc2cf597e0c580
LungPong <54305121+LungPong@users.noreply.github.com>
2021-05-22 00:11:55 +0700

    Update config.js
    change web app on firebase

--- server/src/configs/config.js
+++ server/src/configs/config.js
@@ -3,10 +3,11 @@ const firebase = require("firebase")
 firebase.initializeApp({
   apiKey: "AIzaSyCPyU9speSkRCizTHG8aUKkjPH_3pAvhpI",
   authDomain: "vue-dashboard-wtfss.firebaseapp.com",
+  databaseURL: "https://vue-dashboard-wtfss-default-rtdb.firebaseio.com",
   projectId: "vue-dashboard-wtfss",
   storageBucket: "vue-dashboard-wtfss.appspot.com",
   messagingSenderId: "837863145537",
-  appId: "1:837863145537:web:c0cb9647ca5b373b1cc6c0"
+  appId: "1:837863145537:web:3711909f000a3aa91cc6c0"
 });
 
 const db = firebase.database();
