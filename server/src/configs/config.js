b7b544972a5d1d029691e22d96aec565c6f0078f
LungPong <54305121+LungPong@users.noreply.github.com>
2021-06-15 19:11:16 +0700

    config db again

--- server/src/configs/config.js
+++ server/src/configs/config.js
@@ -1,7 +1,13 @@
 const firebase = require("firebase")
 
 firebase.initializeApp({
-  // Your web app's Firebase configuration
+  apiKey: "AIzaSyCPyU9speSkRCizTHG8aUKkjPH_3pAvhpI",
+  authDomain: "vue-dashboard-wtfss.firebaseapp.com",
+  databaseURL: "https://vue-dashboard-wtfss-default-rtdb.firebaseio.com",
+  projectId: "vue-dashboard-wtfss",
+  storageBucket: "vue-dashboard-wtfss.appspot.com",
+  messagingSenderId: "837863145537",
+  appId: "1:837863145537:web:3711909f000a3aa91cc6c0"
 });
 
 const db = firebase.database();
